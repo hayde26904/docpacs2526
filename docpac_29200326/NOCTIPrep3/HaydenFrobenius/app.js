89fd455eefc16134a21d8f97318b388f1312c0b1
hayde26904 <haydenfrobenius750@gmail.com>
2026-03-23 13:32:49 -0400

    did it

--- docpac_29200326/NOCTIPrep3/HaydenFrobenius/app.js
+++ docpac_29200326/NOCTIPrep3/HaydenFrobenius/app.js
@@ -1,68 +1,43 @@
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
-
-function tableHelper(str, maxLen = 32) {
-    const numSpaces = Math.abs(maxLen - str.length) - 3;
-    let spaces = '';
-    for(i=0;i<numSpaces;i++) spaces += ' ';
-    return str.substring(0, maxLen) + '...' + spaces;
-}
+const fs = require('fs');
+const csv = require('csv-parser');
+const orders = [];
 
 fs.createReadStream('data.csv')
     .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-
-        const orders = [];
-
-        for (i in results){
-
-            const entry = results[i];
-
-            if (entry.customer && entry.address) {
-                orders.push({
-                    customer: entry.customer,
-                    address: entry.address,
-                    items: []
-                });
-            }
-
-            const lastOrder = orders[orders.length - 1];
-
-            const price = Number(entry.price);
-            const quantity = parseInt(entry.quantity)
-            
-            lastOrder.items.push({
-                name: entry.item,
-                quantity,
-                price,
-            });
-
+    .on('data', (data) => {
+        if (orders.length === 0 || data.customer){
+            orders.push({
+                customer: data.customer,
+                address: data.address,
+                items: []
+            })
         }
 
-        for (i in orders) {
-            const order = orders[i];
-
-            let subtotal = 0;
-
-            console.log('-------------------------------------------------------------------------------------');
-            console.log('Name\t'+order.customer);
-            console.log('Address\t'+order.address + '\n');
-            console.log('Item\t\t\t\t\t\tQTY\t\tPrice\t\tTotal');
-            for (item of order.items) {
-                const total = item.price * item.quantity;
-                subtotal += total;
-                console.log(tableHelper(item.name)+'\t\t'+item.quantity+'\t\t'+item.price+'\t\t'+total)
+        orders[orders.length-1].items.push({
+            item: data.item,
+            quantity: parseInt(data.quantity),
+            price: Number(data.price),
+            total: parseInt(data.quantity) * Number(data.price)
+        })
+    })
+    .on('close', () => {
+        let subtotal = 0;
+        for(order of orders) {
+            console.log("------------------------------------------------------------")
+            console.log("Name\t" + order.customer);
+            console.log("Address\t" + order.address + '\n');
+            console.log("Item\t\t\t\t\tPrice\t\tQTY\t\tTotal");
+            for(item of order.items) {
+                console.log(item.item.substring(0,28)+'...'+'\t\t'+item.price+'\t\t'+item.quantity+'\t\t'+item.total);
+                subtotal+=item.total;
             }
-            console.log('\n');
-            const tax = subtotal * 0.06;
-            const shipping = (subtotal > 50) ? 0.00 : 10.00;
-            const grandTotal = subtotal+tax+shipping;
-            console.log('Subtotal\t'+subtotal);
-            console.log('Sales Tax\t'+Number(tax.toFixed(2)));
-            console.log('Shipping\t'+shipping+'.00');
-            console.log('Grand Total\t'+Number(grandTotal.toFixed(2)));
-            console.log('-------------------------------------------------------------------------------------');
+            let tax = subtotal * 0.06;
+            let shipping = subtotal > 50 ? 0.00 : 10.00;
+            let grandTotal = subtotal + tax + shipping;
+            console.log("Subtotal\t"+subtotal);
+            console.log("Sales Tax\t"+tax.toFixed(2));
+            console.log("Shipping\t"+shipping.toFixed(2));
+            console.log("Grand Total\t"+grandTotal.toFixed(2));
+            console.log("------------------------------------------------------------")
         }
-    });
+    })
